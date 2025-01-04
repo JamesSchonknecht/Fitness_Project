59a7a0ce164b9659f25a6d9a756c4d3b1557fee5
JamesSchonknecht <jamesschonknecht@gmail.com>
2025-01-04 19:37:32 +1300

    Update strong_data_cleaning.R
    Script now renames most recent MacroFactor .xlsx file to "MacroFactor_data.xlsx to streamline most recent data import into powerbi. Also enables option to delete old MacroFactor .xlsx files.

--- scripts/strong_data_cleaning.R
+++ scripts/strong_data_cleaning.R
@@ -36,3 +36,30 @@ if (delete_file_choice == "Y") {
   file.remove(csv_path)
   cat(csv_file_name, ".csv", " DELETED", sep = "")
 }
+
+
+# MacroFactor data
+
+# Create character vector of file names and find those matching the MacroFactor... pattern
+macrofactor_filenames <- list.files("../data", full.names = TRUE)
+macrofactor_filenames <- grep("MacroFactor-\\d+", macrofactor_filenames, value = TRUE)
+
+# Macrofactor file information
+macrofactor_file_info <- file.info(macrofactor_filenames)
+
+# Finding most recently created macrofactor .xlsx file
+most_recent_macrofactor <- rownames(macrofactor_file_info)[which.max(macrofactor_file_info$ctime)]
+
+# Updating file name of most recent macrofactor .xlsx file
+file.rename(from = most_recent_macrofactor, to = "../data/MacroFactor_data.xlsx")
+
+# Prompt user to delete all old Macrofactor data files
+cat("Delete all old Macrofactor .xlsx files? (Y/N)")
+delete_macrofactor_files_choice <- toupper(readline())
+
+# Delete all old Macrofactor .xlsx files if "y" or "Y" is entered
+if (delete_macrofactor_files_choice == "Y") {
+  file.remove(macrofactor_filenames)
+}
+
+
